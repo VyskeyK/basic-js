5281ee9bf3bca9012a23a733d131427c871a7419
VyskeyK <robintyan96@gmail.com>
2022-10-13 14:20:21 +0300

    feat: repeat string properly for extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const repeatTimes = 'repeatTimes';
+  
+  let repeatedStr = '';
+  
+  for (let i = 0; i < options[repeatTimes]; i++) {
+    if ( (i + 1) === options[repeatTimes]) {
+      repeatedStr += str;
+    } else {
+      repeatedStr += str + '+';
+    }
+  }
+  
+  return repeatedStr;
 }
 
 module.exports = {
