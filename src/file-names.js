1646b051d7d48703c9e148382b1bae147bda284e
VyskeyK <robintyan96@gmail.com>
2022-10-13 23:26:18 +0300

    feat: add calculations for file names

--- src/file-names.js
+++ src/file-names.js
@@ -15,10 +15,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+  function renameFiles(names) {
+    let files = {};
+    
+    for (let i = 0; i < names.length; i++) {
+      if (files.hasOwnProperty(names[i])) {
+        files[names[i]] += 1;
+        names[i] = (names[i] + `(${files[names[i]]})`);
+        files[names[i]] = 0;
+      } else {
+        files[names[i]] = 0;
+        names[i] = (names[i]);
+      }
+    }
+    
+    return names;
+  }
 
 module.exports = {
   renameFiles
