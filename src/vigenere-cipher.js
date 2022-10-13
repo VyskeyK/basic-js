ffa63d19796dbc56a7c9d984bdd76b57367bc6da
VyskeyK <robintyan96@gmail.com>
2022-10-13 18:01:03 +0300

    feat: add base decryption for vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -29,15 +29,17 @@ class VigenereCipheringMachine {
     const messageArr = message.toLowerCase().split('')
     const messageToAlphabet = messageArr.map(letter => letter.charCodeAt(0) - 96);
     const keyArr = key.toLowerCase().split('');
-    const keyToAlphabet = keyArr.map(letter => letter.charCodeAt(0) - 96);
+    const keyToAlphabetArr = keyArr.map(letter => letter.charCodeAt(0) - 96);
     const keyLength = keyArr.length;
     
     let keyToInsert = 0;
+    let keyToAlphabet = [];
+    
     for (let i = 0; i < messageToAlphabet.length; i ++) {
       if (messageToAlphabet[i] <=0) {
         keyToAlphabet.splice(i, 0, messageToAlphabet[i]);
       } else if (keyToAlphabet.length <= i) {
-        keyToAlphabet.push(keyToAlphabet[keyToInsert % keyLength]);
+        keyToAlphabet.push(keyToAlphabetArr[keyToInsert % keyLength]);
         keyToInsert += 1;
       }      
     }
@@ -56,9 +58,41 @@ class VigenereCipheringMachine {
     
     return encryptMessage.join('');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    const messageArr = message.toLowerCase().split('')
+    const messageToAlphabet = messageArr.map(letter => letter.charCodeAt(0) - 96);
+    const keyArr = key.toLowerCase().split('');
+    const keyToAlphabetArr = keyArr.map(letter => letter.charCodeAt(0) - 96);
+    const keyLength = keyArr.length;
+    
+    let keyToInsert = 0;
+    let keyToAlphabet = [];
+
+    for (let i = 0; i < messageToAlphabet.length; i ++) {
+      if (messageToAlphabet[i] <=0) {
+        keyToAlphabet.splice(i, 0, messageToAlphabet[i]);
+      } else if (keyToAlphabet.length <= i) {
+        keyToAlphabet.push(keyToAlphabetArr[keyToInsert % keyLength]);
+        keyToInsert += 1;
+      }
+    }
+
+    let decryptMessage = [];
+    let decryptAlphabet = [];
+    for (let i = 0; i < messageToAlphabet.length; i++) {
+      if (messageToAlphabet[i] <= 0) {
+        decryptAlphabet.push(keyToAlphabet[i]);
+      } else {
+        if (messageToAlphabet[i] >= keyToAlphabet[i])
+          decryptAlphabet.push((messageToAlphabet[i] - keyToAlphabet[i]+1) % 26);
+        else
+          decryptAlphabet.push(26 - (keyToAlphabet[i] - messageToAlphabet[i]-1));
+      }
+    }
+    
+    decryptMessage = decryptAlphabet.map(letter => String.fromCharCode(letter+96).toUpperCase());
+    
+    return decryptMessage.join('');
   }
 }
 
