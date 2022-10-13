84c13649be0e11b911c615517392b5d94337ffc0
VyskeyK <robintyan96@gmail.com>
2022-10-13 15:39:34 +0300

    feat: add multi words support for extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -19,22 +19,33 @@ function repeater(str, options) {
   const repeatTimes = 'repeatTimes';
   const separator = 'separator';
   const addition = 'addition';
-  const additionRepeatTimes = options['additionRepeatTimes'];
-
+  const additionRepeatTimes = 'additionRepeatTimes';
+  const additionSeparator = 'additionSeparator';
+  
+  let repeatedStr = '';
   const repeatTimesNum = options[repeatTimes] === undefined ? 1 : options[repeatTimes];
   const separatorStr = options[separator] === undefined ? '+' : options[separator];
+  const additionSeparatorStr = options[additionSeparator] === undefined ? '' : options[additionSeparator];
+  const additionRepeatTimesNum = options[additionRepeatTimes] === undefined ? 1 : options[additionRepeatTimes];
   let additionStr = options[addition] === undefined ? '' : options[addition];
-  let repeatedStr = '';
-
-  if (additionRepeatTimes !== undefined) additionStr.repeat(additionRepeatTimes);
-
-  if (repeatTimesNum === 1) return str + additionStr;
+  let fullAdditionStr = '';
   
+  for (let j = 0; j < additionRepeatTimesNum; j++) {
+    if ( (j + 1) === additionRepeatTimesNum) {
+      fullAdditionStr += additionStr;
+    } else {
+      fullAdditionStr += additionStr + additionSeparatorStr;
+    }
+  }
+
+
+  if (repeatTimesNum === 1) return str + fullAdditionStr;
+
   for (let i = 0; i < repeatTimesNum; i++) {
     if ( (i + 1) === repeatTimesNum) {
-      repeatedStr += str + additionStr;
+      repeatedStr += str + fullAdditionStr;
     } else {
-      repeatedStr += str + additionStr + separatorStr;
+      repeatedStr += str + fullAdditionStr + separatorStr;
     }
   }
   
