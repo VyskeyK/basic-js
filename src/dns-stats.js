fa1901bc4d6e21beedad473dc0db4afeb61cf98f
VyskeyK <robintyan96@gmail.com>
2022-10-13 22:38:41 +0300

    feat: add calculations fot dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  function getDNSStats(domains) {
+    let result = {};
+
+    domains.forEach( (domain) => {
+      domain = domain.split(".").reverse();
+      let key = '';
+      
+      domain.forEach( (item) => {
+        key = key + '.' + item;
+        result.hasOwnProperty(key) ? result[key] += 1: result[key] = 1;
+      })
+    })
+    
+    return(result);
 }
 
 module.exports = {
