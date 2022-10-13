5e6df3c8949dae6b378c0eb8e348dcd11d771da4
VyskeyK <robintyan96@gmail.com>
2022-10-13 22:28:22 +0300

    feat: add calculations for delete digits

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+  function deleteDigit(n) {  
+    n = ''+n;
+    let combinationArr = [];
+    
+    for (let i = 0; i < n.length; i++) {
+      combinationArr.push(+(n.slice(0, i) + n.slice(i + 1)));
+    }
+    
+    return Math.max(...combinationArr);
+  }
 
 module.exports = {
   deleteDigit
