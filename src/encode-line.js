5a2ca991c4f8a9d7d7cc7734b1cd22ed7118c755
VyskeyK <robintyan96@gmail.com>
2022-10-13 23:10:50 +0300

    feat: add calculations for encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+  function encodeLine(str) {
+    str = str.split('');
+    let result = '';
+
+    for (let i = 0; i < str.length; i++) {
+      let count = 1;
+      
+      while (str[i] === str[i + 1]) {
+        count++;
+        i++;
+      }
+      
+      result += count > 1 ? count + str[i]: str[i];
+    }
+  
+    return result;
+  }
 
 module.exports = {
   encodeLine
