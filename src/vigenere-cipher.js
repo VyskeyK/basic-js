5ab504e4b67f5befb5b741026cbbde96168df44a
VyskeyK <robintyan96@gmail.com>
2022-10-13 17:34:22 +0300

    feat: add base encryption for vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,9 +20,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direct = true) {
+    this.direct = direct;
+  }
+  encrypt(message, key) {
+    console.log(message.length);
+    console.log(key.length);
+    const messageArr = message.toLowerCase().split('')
+    const messageToAlphabet = messageArr.map(letter => letter.charCodeAt(0) - 96);
+    const keyArr = key.toLowerCase().split('');
+    const keyToAlphabet = keyArr.map(letter => letter.charCodeAt(0) - 96);
+    const keyLength = keyArr.length;
+    
+    let keyToInsert = 0;
+    for (let i = 0; i < messageToAlphabet.length; i ++) {
+      if (messageToAlphabet[i] <=0) {
+        keyToAlphabet.splice(i, 0, messageToAlphabet[i]);
+      } else if (keyToAlphabet.length <= i) {
+        keyToAlphabet.push(keyToAlphabet[keyToInsert % keyLength]);
+        keyToInsert += 1;
+      }      
+    }
+    
+    let encryptMessage = [];
+    let encryptAlphabet = [];
+    for (let i = 0; i < messageToAlphabet.length; i++) {
+      if (messageToAlphabet[i] <= 0) {
+        encryptAlphabet.push(keyToAlphabet[i]);
+      } else {
+        encryptAlphabet.push((messageToAlphabet[i] + keyToAlphabet[i] -1) % 26);
+      }
+    }
+
+    encryptMessage = encryptAlphabet.map(letter => String.fromCharCode(letter+96).toUpperCase());
+    
+    return encryptMessage.join('');
   }
   decrypt() {
     throw new NotImplementedError('Not implemented');
